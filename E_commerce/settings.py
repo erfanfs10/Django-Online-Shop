4ba7b3cb172c4e4ac9596a5db376b454261b8fc7
erfan <mahdi.farahmandsafa@gmail.com>
2023-05-11 16:12:46 +0330

    update media and static url

--- E_commerce/settings.py
+++ E_commerce/settings.py
@@ -140,11 +140,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'eco/static/'
 STATIC_ROOT =  BASE_DIR / 'static'
 STATICFILES_DIR = BASE_DIR, "static"
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'eco/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 LOGIN_URL="login"
